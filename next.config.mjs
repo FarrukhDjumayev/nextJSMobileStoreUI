e3fb5f76e73c8e8b2182e1cf82aad3048d055951
FarrukhDjumayev <farrukhdjumayev0542@gmail.com>
2025-04-03 15:26:06 +0500

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "cdn.dummyjson.com",
-            pathname: "/products/**",
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
+        pathname: "/products/**",
       },
+    ],
+  },
 };
 
 export default nextConfig;
